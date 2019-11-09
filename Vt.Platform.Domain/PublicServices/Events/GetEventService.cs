17867a12cb572d896e7851b147d943b0c2a00e2b
Michael Gattuso <michael.gattuso@gmail.com>
2019-11-09 12:47:36 -0500

    Added stub GetEvent service

---
+++ Vt.Platform.Domain/PublicServices/Events/GetEventService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Vt.Platform.Domain.PublicServices.System;
+using Vt.Platform.Utils;
+
+namespace Vt.Platform.Domain.PublicServices.Events
+{
+    public class GetEventService : BaseService<GetEventService.Request, GetEventService.Response>
+    {
+        public GetEventService(ILogger logger) : base(logger)
+        {
+        }
+
+        protected override async Task<Response> Implementation(Request request)
+        {
+            await Task.CompletedTask;
+
+            return new Response
+            {
+                EventCode = request.EventCode,
+                OrganizerName = "John Smith",
+                Summary = "Bearcats Homecoming Parade",
+                Details = "Come join the bear cats parade",
+                NumberOfParticipantsRequested = 100,
+                EventDate = DateTime.Now
+            };
+        }
+
+        public override IDictionary<int, string> GetErrorCodes()
+        {
+            return new Dictionary<int, string>();
+        }
+
+
+        public class Request : BaseRequest
+        {
+            public string EventCode { get; set; }
+        }
+
+        public class Response : BaseResponse
+        {
+            public string EventCode { get; set; }
+            public string OrganizerName { get; set; }
+            public DateTime EventDate { get; set; }
+            public string Summary { get; set; }
+            public string Details { get; set; }
+            public int NumberOfParticipantsRequested { get; set; }
+        }
+
+    }
+}
---
+++ Vt.Platform.Functions/HttpTriggers/Events/GetEventFunction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Tokenize.Client;
+using Vt.Platform.Domain.PublicServices.Events;
+using Vt.Platform.Domain.PublicServices.System;
+using Vt.Platform.Utils;
+
+namespace Vt.Platform.Functions.HttpTriggers.Events
+{
+    public class GetEventFunction
+    {
+        private readonly ILogger<GetEventFunction> _logger;
+        private readonly IObjectTokenizer _objectTokenizer;
+
+        public GetEventFunction(
+            ILogger<GetEventFunction> logger,
+            IObjectTokenizer objectTokenizer)
+        {
+            _logger = logger;
+            _objectTokenizer = objectTokenizer;
+        }
+
+        [FunctionName("GetEvent")]
+        public async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestMessage req)
+        {
+            var response = await req.CallService(
+                () => new GetEventService(_logger),
+                _objectTokenizer,
+                HttpMethod.Get,
+                HttpMethod.Post);
+            return response;
+        }
+    }
+}
