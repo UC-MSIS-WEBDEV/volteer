5681482b9ee616e2b987905ebf22e9356e19590d
Michael Gattuso <michael.gattuso@gmail.com>
2019-11-14 21:29:20 -0500

    Remove JS based SSL redirect

--- Vt.Platform.Web/wwwroot/js/site.js
+++ Vt.Platform.Web/wwwroot/js/site.js
@@ -2,8 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-
-if (location.protocol != 'https:')
-{
- location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-}
